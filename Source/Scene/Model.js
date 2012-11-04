ed11b5fdb774a83d6b00a5a70be44cec91fc0d67
Patrick Cozzi <pjcozzi@gmail.com>
2012-11-04 16:40:33 -0500

    Actual start of model

---
+++ Source/Scene/Model.js
@@ -0,0 +1,124 @@
+/*global define*/
+define([
+        '../Core/Matrix4',
+        '../Renderer/CommandLists',
+        '../Renderer/DrawCommand',
+        './SceneMode'
+    ], function(
+        Matrix4,
+        CommandLists,
+        DrawCommand,
+        SceneMode) {
+    "use strict";
+
+    /**
+     * DOC_TBA
+     *
+     * @alias Model
+     * @constructor
+     */
+    var Model = function() {
+        debugger;
+
+        /**
+         * The 4x4 transformation matrix that transforms the model from model to world coordinates.
+         * When this is the identity matrix, the model is drawn in world coordinates, i.e., Earth's WGS84 coordinates.
+         * Local reference frames can be used by providing a different transformation matrix, like that returned
+         * by {@link Transforms.eastNorthUpToFixedFrame}.  This matrix is available to GLSL vertex and fragment
+         * shaders via {@link czm_model} and derived uniforms.
+         * <p>
+         * The default is {@link Matrix4.IDENTITY}.
+         * </p>
+         *
+         * @type Matrix4
+         *
+         * @example
+         * var origin = ellipsoid.cartographicToCartesian(
+         *   Cartographic.fromDegrees(-95.0, 40.0, 200000.0));
+         * m.modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
+         *
+         * @see Transforms.eastNorthUpToFixedFrame
+         * @see czm_model
+         */
+        this.modelMatrix = Matrix4.IDENTITY.clone();
+        this._computedModelMatrix = Matrix4.IDENTITY.clone();
+
+        /**
+         * Determines if the model primitive will be shown.
+         * <p>
+         * The default is <code>true</code>.
+         * </p>
+         *
+         * @type Boolean
+         */
+        this.show = true;
+
+        // new DrawCommand();
+        this._colorCommands = [];
+        this._commandLists = new CommandLists();
+    };
+
+    /**
+     * @private
+     *
+     * @exception {DeveloperError} this.material must be defined.
+     */
+    Model.prototype.update = function(context, frameState, commandList) {
+        debugger;
+
+        if (!this.show ||
+            (frameState.mode !== SceneMode.SCENE3D)) {
+            return;
+        }
+
+        var modelCommandLists = this._commandLists;
+        modelCommandLists.removeAll();
+
+        if (frameState.passes.color) {
+            modelCommandLists.colorList = this._colorCommands;
+        }
+
+        commandList.push(modelCommandLists);
+    };
+
+    /**
+     * Returns true if this object was destroyed; otherwise, false.
+     * <br /><br />
+     * If this object was destroyed, it should not be used; calling any function other than
+     * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.
+     *
+     * @memberof Model
+     *
+     * @return {Boolean} <code>true</code> if this object was destroyed; otherwise, <code>false</code>.
+     *
+     * @see Model#destroy
+     */
+    Model.prototype.isDestroyed = function() {
+        return false;
+    };
+
+    /**
+     * Destroys the WebGL resources held by this object.  Destroying an object allows for deterministic
+     * release of WebGL resources, instead of relying on the garbage collector to destroy this object.
+     * <br /><br />
+     * Once an object is destroyed, it should not be used; calling any function other than
+     * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.  Therefore,
+     * assign the return value (<code>undefined</code>) to the object as done in the example.
+     *
+     * @memberof Model
+     *
+     * @return {undefined}
+     *
+     * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
+     *
+     * @see Model#isDestroyed
+     *
+     * @example
+     * e = e && e.destroy();
+     */
+    Model.prototype.destroy = function() {
+        return destroyObject(this);
+    };
+
+    return Model;
+});
