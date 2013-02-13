c49cadbb4404321cfdbf7ca3beb74b3d21a82414
Matthew Amato <matt.amato@gmail.com>
2013-02-13 14:11:11 -0500

    First cut at a version showing RBSP satellites.

--- Apps/CesiumViewer/CesiumViewer.js
+++ Apps/CesiumViewer/CesiumViewer.js
@@ -18,6 +18,9 @@ define([
     "use strict";
     /*global console*/
 
+    function doNothing() {
+    }
+
     ready(function() {
         parser.parse();
 
@@ -27,30 +30,79 @@ define([
         if (window.location.search) {
             endUserOptions = ioQuery.queryToObject(window.location.search.substring(1));
         }
-        endUserOptions.source = 'Gallery/DA14/2012_DA14_CA.czml';
+
+        var rbsp = typeof endUserOptions.RBSP !== 'undefined';
+        if (rbsp) {
+            endUserOptions.source = 'Gallery/DA14/2012_DA14_RBSP.czml';
+        } else {
+            endUserOptions.source = 'Gallery/DA14/2012_DA14.czml';
+        }
 
         var widget = new CesiumViewerWidget({
             endUserOptions : endUserOptions,
             enableDragDrop : true
         });
+
         widget.enableDragDrop = false;
         widget.fullscreenElement = document.body;
         widget.placeAt('cesiumContainer');
         widget.imagery.domNode.style.display = 'none';
-        widget.onObjectSelected = function() {
-        };
+        widget.viewHomeButton.domNode.style.display = 'none';
+        widget.onObjectSelected = doNothing;
         widget.startup();
 
-        domClass.remove(win.body(), 'loading');
+        var asteroidOverlay = document.getElementById('asteroidOverlayId');
+        asteroidOverlay.style.display = 'block';
 
         var zoomButton = document.getElementById('zoomID');
-        zoomButton.onclick = function() {
-            var lookAtObject = widget.dynamicObjectCollection.getObject('/Application/STK/Scenario/2012_DA14_CA/Satellite/2012_DA14');
-            if (typeof widget._viewFromTo === 'undefined' || widget._viewFromTo.dynamicObject !== lookAtObject) {
-                widget.centerCameraOnObject(lookAtObject);
-            } else {
-                widget.viewHome();
-            }
-        };
+        var viewButton = document.getElementById('viewButtonId');
+
+        if (rbsp) {
+            viewButton.style.display = 'block';
+            zoomButton.style.display = 'none';
+            var rbspId = document.getElementById('rbspId');
+
+            viewButton.onchange = function(e) {
+                if (e.srcElement.value === 'Home') {
+                    asteroidOverlay.style.display = 'block';
+                    rbspId.style.display = 'none';
+                    widget.viewHome();
+                    return;
+                }
+                var lookAtObject;
+                if (e.srcElement.value === 'DA14') {
+                    asteroidOverlay.style.display = 'block';
+                    rbspId.style.display = 'none';
+                    lookAtObject = widget.dynamicObjectCollection.getObject('/Application/STK/Scenario/2012_DA14_CA/Satellite/2012_DA14');
+                }
+                else if (e.srcElement.value === 'RBSP A') {
+                    asteroidOverlay.style.display = 'none';
+                    rbspId.style.display = 'block';
+                    lookAtObject = widget.dynamicObjectCollection.getObject('/Application/STK/Scenario/2012_DA14_CA/Satellite/RBSP_A');
+                }
+                else if (e.srcElement.value === 'RBSP B') {
+                    asteroidOverlay.style.display = 'none';
+                    rbspId.style.display = 'block';
+                    lookAtObject = widget.dynamicObjectCollection.getObject('/Application/STK/Scenario/2012_DA14_CA/Satellite/RBSP_B');
+                }
+
+                if (typeof widget._viewFromTo === 'undefined' || widget._viewFromTo.dynamicObject !== lookAtObject) {
+                    widget.centerCameraOnObject(lookAtObject);
+                }
+            };
+        } else {
+            viewButton.style.display = 'none';
+            zoomButton.style.display = 'block';
+            zoomButton.onclick = function() {
+                var lookAtObject = widget.dynamicObjectCollection.getObject('/Application/STK/Scenario/2012_DA14_CA/Satellite/2012_DA14');
+                if (typeof widget._viewFromTo === 'undefined' || widget._viewFromTo.dynamicObject !== lookAtObject) {
+                    widget.centerCameraOnObject(lookAtObject);
+                } else {
+                    widget.viewHome();
+                }
+            };
+        }
+
+        domClass.remove(win.body(), 'loading');
     });
 });
